8f456cbcda159da7743c6c8d309ba7b20a9bded9
So <wolfiaspikky@gmail.com>
2023-01-10 14:03:13 +0100

    method for depose

--- poo/class/Compte.php
+++ poo/class/Compte.php
@@ -8,13 +8,14 @@ class Compte {
      * contructeur du compte bancaire
      *
      * @param string $nom nom du titulaire
+     * @param float $montant du solde
      */
     public function __construct(string $nom,float $montant = 100)
     {
         // on attribue le nom à la propriété titulaire de l'instance crée
         $this->titulaire = $nom; 
         //on attribue le montant à la propriété solde
-        $this->solde = $montant
+        $this->solde = $montant;
     }
 
     // Propriétés
@@ -31,4 +32,16 @@ class Compte {
      */
     public $solde;
 
+    /**
+     * Déposer de l'argent sur le compte
+     *
+     * @param float $montant montant depose
+     * @return void
+     */
+    public function deposer(float $montant) {
+        // on verifie si le montant est positif
+        if($montant > 0){
+            $this->solde += $montant;
+        }
+    }
 }
--- poo/index.php
+++ poo/index.php
@@ -2,11 +2,15 @@
 require_once 'class/Compte.php';
 
 // on instancie le compte
-$compte1 = new Compte('sarah');
+$compte1 = new Compte('sarah', 500);
 // on ecrit dans la propriété titulaire
 // $compte1->titulaire = "sarah";
 // on ecrit dans la propriété solde
-$compte1-> solde = 500;
+// $compte1-> solde = 500;
+
+// on dépose 100 euros
+$compte1->deposer(100);
+
 
 var_dump($compte1);
 
