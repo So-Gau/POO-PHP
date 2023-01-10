e7ca440ce83564b2bf18c0deebb4b94d168a9ef2
So <wolfiaspikky@gmail.com>
2023-01-10 13:52:57 +0100

    create contructor

--- poo/class/Compte.php
+++ poo/class/Compte.php
@@ -3,17 +3,32 @@
  * Objet compte bancaire
  */
 class Compte {
+
+    /**
+     * contructeur du compte bancaire
+     *
+     * @param string $nom nom du titulaire
+     */
+    public function __construct(string $nom,float $montant = 100)
+    {
+        // on attribue le nom à la propriété titulaire de l'instance crée
+        $this->titulaire = $nom; 
+        //on attribue le montant à la propriété solde
+        $this->solde = $montant
+    }
+
     // Propriétés
     /**
      * titulaire du compte
      *
      * @var string
      */
-    public $titutlaire;
+    public $titulaire;
     /**
      * solde du compte
      *
      * @var [float]
      */
     public $solde;
+
 }
--- poo/index.php
+++ poo/index.php
@@ -2,16 +2,16 @@
 require_once 'class/Compte.php';
 
 // on instancie le compte
-$compte1 = new compte;
+$compte1 = new Compte('sarah');
 // on ecrit dans la propriété titulaire
-$compte1->titutlaire = "sarah";
+// $compte1->titulaire = "sarah";
 // on ecrit dans la propriété solde
 $compte1-> solde = 500;
 
 var_dump($compte1);
 
-$compte2 = new compte;
-$compte2->titutlaire = "lulu";
+$compte2 = new Compte('lulu');
+// $compte2->titulaire = "lulu";
 $compte2->solde = 352.25;
 
  var_dump($compte2);
