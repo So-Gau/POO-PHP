76fdd5561f4c8d3fe3f35bee6a744875c6c878bf
So <wolfiaspikky@gmail.com>
2023-01-10 13:44:27 +0100

    test instance

--- poo/index.php
+++ poo/index.php
@@ -5,9 +5,13 @@ require_once 'class/Compte.php';
 $compte1 = new compte;
 // on ecrit dans la propriété titulaire
 $compte1->titutlaire = "sarah";
+// on ecrit dans la propriété solde
+$compte1-> solde = 500;
 
 var_dump($compte1);
 
 $compte2 = new compte;
 $compte2->titutlaire = "lulu";
+$compte2->solde = 352.25;
+
  var_dump($compte2);
